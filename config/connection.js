55942c9953a91f063c6b1e8451a14ef4d69e3e92
EthanP94 <ethan.plott@yahoo.com>
2022-05-15 15:49:21 -0700

    finished

--- config/connection.js
+++
@@ -1,8 +0,0 @@
-const { connect, connection } = require('mongoose');
-
-connect('mongodb://localhost/TheSocialDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = connection;
